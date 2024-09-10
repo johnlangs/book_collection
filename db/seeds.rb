089efe5f64b05c0a14b303d4db5c3b50b4775ce9
johnlangs <me@johnlangs.dev>
2024-09-10 00:59:43 +0000

    added db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+Book.create!([
+               { title: 'Book 1' },
+               { title: 'Book 2' },
+               { title: 'Book 3' },
+               { title: 'Book 4' },
+               { title: 'Book 5' }
+             ])
